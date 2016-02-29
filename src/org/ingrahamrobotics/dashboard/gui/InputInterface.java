bfb71b897f69dfb1ce30ea2686c3140641b09007
Zachary Kotlarek <zach@kotlarek.com>
2016-02-28 23:58:08 -0800

    Add scrolling to parameters pane

--- src/org/ingrahamrobotics/dashboard/gui/InputInterface.java
+++ src/org/ingrahamrobotics/dashboard/gui/InputInterface.java
@@ -54,7 +54,9 @@ public class InputInterface implements InputListener {
         // tableRootPanel
         tableRootPanel = new JPanel(new GridBagLayout());
         tableRootPanel.setMinimumSize(new Dimension(600, 40));
-        application.getRoot().getMainPanel().add(tableRootPanel, new GBC().weightx(1).weighty(1).fill(GridBagConstraints.BOTH).gridx(1).gridy(1));
+        JScrollPane scrollFrame = new JScrollPane(tableRootPanel);
+        //scrollFrame.setPreferredSize(new Dimension(600, 40));
+        application.getRoot().getMainPanel().add(scrollFrame, new GBC().weightx(1).weighty(1).fill(GridBagConstraints.BOTH).gridx(1).gridy(1));
 
         // tableRootPanel refresh
         tableRootPanel.revalidate();
