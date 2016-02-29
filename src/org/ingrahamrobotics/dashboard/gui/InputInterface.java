d6e2e6f291b7fc95bd554ddfbbd9d1ae79bd7ab2
Zachary Kotlarek <zach@kotlarek.com>
2016-02-29 00:44:06 -0800

    Sort the parameter list

--- src/org/ingrahamrobotics/dashboard/gui/InputInterface.java
+++ src/org/ingrahamrobotics/dashboard/gui/InputInterface.java
@@ -23,7 +23,6 @@ import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.List;
